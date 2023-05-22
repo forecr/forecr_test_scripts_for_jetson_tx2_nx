f410e75ac98f06ec02a6a42b99f134580a4dccbf
mdeniz0 <mehmetdeniz@mistelektronik.com>
2023-05-22 10:05:28 +0300

    Interrupt functions added in CANBus, digital-in and M.2 Key-B script files

--- test_key_b_tx2_nx.sh
+++ test_key_b_tx2_nx.sh
@@ -1,5 +1,10 @@
 sudo echo 425 > /sys/class/gpio/export
 sudo echo out > /sys/class/gpio/gpio425/direction
+
+trap interrupt_func INT
+interrupt_func() {
+	sudo echo 425 > /sys/class/gpio/unexport
+}
+
 watch -n 0.1 lsusb
-sudo echo 425 > /sys/class/gpio/unexport
 
